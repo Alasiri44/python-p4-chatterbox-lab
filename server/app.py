73150a8bd375e229033b849a2f4ca1018e21c814
alasiri <austinalasiri44@gmail.com>
2025-06-18 00:35:44 +0300

    passed all tests

--- server/app.py
+++ server/app.py
@@ -14,13 +14,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+# GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+# POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method == 'GET':
+        response_body = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        status_code = 200
+        
+    elif request.method == 'POST':
+        
+        new_message = Message(
+            body = request.get_json().get('body'),
+            username = request.get_json().get('username')
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response_body = new_message.to_dict()
+        status_code = 201
+        
+    return make_response(response_body, status_code)
 
-@app.route('/messages/<int:id>')
+# PATCH /messages/<int:id>: updates the body of the message using params, and returns the updated message as JSON.
+# DELETE /messages/<int:id>: deletes the message from the database.
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    body = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(body, attr, request.get_json().get(attr))
+        db.session.commit()
+        response_body = body.to_dict()
+        status_code = 200
+        
+    elif request.method == 'DELETE':
+        db.session.delete(body)
+        db.session.commit()
+        response_body = {'message': 'The record has been successfully deleted'}
+        status_code = 200
+        
+    return make_response(response_body, status_code)
 
 if __name__ == '__main__':
     app.run(port=5555)
